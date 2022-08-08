9ae1d9ed82570ed95d534c5199064000c4635a26
Kenty Akoarum <kamome-black@akoarum.net>
2022-08-08 12:49:47 +0900

    add input type and form validation

--- src/runtime/components/spearly-form/types.ts
+++ src/runtime/components/spearly-form/types.ts
@@ -3,6 +3,7 @@ import { SpearlyForm } from '../../types'
 export type SpearlyFormState = {
   form: { createdAt: Date | null } & Omit<SpearlyForm, 'createdAt'>
   answers: { [key: string]: string | string[]; _spearly_gotcha: string }
+  errors: Map<string, string>
   error: boolean
   validateError: boolean
   confirm: boolean
