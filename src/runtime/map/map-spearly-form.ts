d01ea6dd1c891fbd048481b50f6e9189340e9893
Kenty Akoarum <kamome-black@akoarum.net>
2023-03-24 09:11:41 +0900

    add new params into form

--- src/runtime/map/map-spearly-form.ts
+++ src/runtime/map/map-spearly-form.ts
@@ -1,10 +1,30 @@
 import { SpearlyForm, ServerSpearlyForm } from '../types'
 
 export const mapSpearlyForm = (form: ServerSpearlyForm): SpearlyForm => {
+  const {
+    confirmationEmailEnabled,
+    confirmationEmailName,
+    confirmationEmailDescription,
+    confirmationScreenBeforeSubmitEnabled,
+    backButtonLabel,
+    submitButtonLabel,
+    ...others
+  } = form
+
   return {
-    ...form,
+    ...others,
     startedAt: form.startedAt ? new Date(form.startedAt) : null,
     endedAt: form.endedAt ? new Date(form.endedAt) : null,
     createdAt: new Date(form.createdAt),
+    confirmationEmail: {
+      enabled: confirmationEmailEnabled,
+      name: confirmationEmailName,
+      description: confirmationEmailDescription,
+    },
+    confirmationScreen: {
+      enabled: confirmationScreenBeforeSubmitEnabled,
+      backButtonLabel: backButtonLabel || '',
+      submitButtonLabel: submitButtonLabel || '',
+    },
   }
 }
